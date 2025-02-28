7b095370443898ea72e2f35339f5a3f32f6721f8
DIVEN009 <sainidiven@gmail.com>
2025-02-28 09:45:58 +0530

    done

---
+++ my-react-app/src/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Diven Saini</h1>
+      <p>Contact: +91 XXXXXXXXXX</p>
+      <p>Email: diven@example.com</p>
+    </div>
+  );
+};
+
+export default Home;
